b34441060d711a711f1f7d9e8f5ad751f21f6e9a
Rishabh bhatia <rishabh.bhatia08@gmail.com>
2016-10-23 16:51:23 +0530

    Adding new iWantUtilities

--- app/src/main/java/com/statiate/iwantto/utils/iWantUtils.java
+++ app/src/main/java/com/statiate/iwantto/utils/iWantUtils.java
@@ -1,5 +1,11 @@
 package com.statiate.iwantto.utils;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.view.Display;
+
 import java.util.Random;
 
 /**
@@ -13,4 +19,47 @@ public class iWantUtils {
     {
         return new Random().nextInt((max - min) + 1) + min;
     }
+
+    public static String generateRandomString(int length)
+    {
+        char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            char c = chars[random.nextInt(chars.length)];
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    public static int getDominantColor(Bitmap bitmap)
+    {
+        Bitmap newBitmap = Bitmap.createScaledBitmap(bitmap, 1, 1, true);
+        final int color = newBitmap.getPixel(0, 0);
+        newBitmap.recycle();
+        return color;
+    }
+
+    public static int getScreenHeight(Activity activity)
+    {
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        return size.y;
+    }
+
+    public static int getScreenWidth(Activity activity)
+    {
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        return size.x;
+    }
+
+    public static int getScreenPixels(Context context, int dps)
+    {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dps * scale + 0.5f);
+    }
+
 }
