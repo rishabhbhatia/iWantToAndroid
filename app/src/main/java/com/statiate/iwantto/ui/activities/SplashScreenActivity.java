b617d665c6bf56f1f9a682bf6858afe1faaa1ac9
Rishabh bhatia <rishabh.bhatia08@gmail.com>
2016-10-25 01:42:23 +0530

    Splash Screen finish particle animation

--- app/src/main/java/com/statiate/iwantto/ui/activities/SplashScreenActivity.java
+++ app/src/main/java/com/statiate/iwantto/ui/activities/SplashScreenActivity.java
@@ -1,6 +1,8 @@
 package com.statiate.iwantto.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -8,6 +10,8 @@ import android.widget.ImageView;
 import com.q42.android.scrollingimageview.ScrollingImageView;
 import com.statiate.iwantto.R;
 import com.statiate.iwantto.base.iWantToActivity;
+import com.yalantis.starwars.TilesFrameLayout;
+import com.yalantis.starwars.interfaces.TilesFrameLayoutListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,7 +20,7 @@ import butterknife.ButterKnife;
  * Created by Rishabh Bhatia on 25/10/16.
  */
 
-public class SplashScreen extends iWantToActivity {
+public class SplashScreenActivity extends iWantToActivity {
 
     @BindView(R.id.siv_splash_background)
     ScrollingImageView sivSplashBackground;
@@ -26,6 +30,8 @@ public class SplashScreen extends iWantToActivity {
     ImageView ivSplashAnimCharacter;
     @BindView(R.id.frame_splash_scroll_animation_holder)
     FrameLayout frameSplashScrollAnimationHolder;
+    @BindView(R.id.tiles_frame_splash)
+    TilesFrameLayout tilesFrameLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,5 +41,24 @@ public class SplashScreen extends iWantToActivity {
 
         sivSplashBackground.start();
         sivSplashForeground.start();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                tilesFrameLayout.startAnimation();
+                tilesFrameLayout.setOnAnimationFinishedListener(new TilesFrameLayoutListener() {
+                    @Override
+                    public void onAnimationFinished() {
+                        startGoalSelectorScreen();
+                    }
+                });
+            }
+        }, 5000);
+    }
+
+    private void startGoalSelectorScreen() {
+        Intent intent = new Intent(SplashScreenActivity.this, GoalSelectorActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
